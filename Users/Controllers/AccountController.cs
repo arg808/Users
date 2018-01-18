171ef3b777ae2485c820c384ad7855a1011a75ce
ag3929 <ag3929@FLCDTL03AG3929>
2018-01-18 17:53:12 -0500

    Chapter 14 - 1

---
+++ Users/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Users.Models;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity.Owin;
+using Users.Infrastructure;
+
+
+
+namespace Users.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel details, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await UserManager.FindAsync(details.Name, details.Password);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Invalid name or password.");
+                }
+                else
+                {
+                    ClaimsIdentity ident = await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    AuthManager.SignOut();
+                    AuthManager.SignIn(new AuthenticationProperties { IsPersistent = false }, ident);
+
+                    return Redirect(returnUrl);
+                }
+            }
+
+
+            return View(details);
+        }
+
+
+        private IAuthenticationManager AuthManager
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
+        }
+
+        private AppUserManager UserManager
+        {
+            get { return HttpContext.GetOwinContext().GetUserManager<AppUserManager>(); }
+        }
+
+    }
+}
