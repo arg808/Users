5227bff5f66e25c4426ded3fae1d4d3933a72ec2
Aaron Guidry <aguidry3@gatech.edu>
2018-01-17 23:57:43 -0500

    Finished Chapter 13

--- Users/Infrastructure/AppUserManager.cs
+++ Users/Infrastructure/AppUserManager.cs
@@ -21,6 +21,21 @@ namespace Users.Infrastructure
             AppIdentityDbContext db = context.Get<AppIdentityDbContext>();
             AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
 
+            manager.PasswordValidator = new CustomPasswordValidator()
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = true,
+                RequireUppercase = true
+            };
+
+            manager.UserValidator = new CustomUserValidator(manager)
+            {
+                AllowOnlyAlphanumericUserNames = true,
+                RequireUniqueEmail = true
+            };
+
             return manager;
         }
 
